9e1f62a706e4902101193852467e2aedd95bf038
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-03-22 13:00:31 +0530

    find student updated

--- index.js
+++ index.js
@@ -29,72 +29,70 @@ app.get("/student", async function (req, res) {
 });
 
 app.get("/mentor", async function (req, res) {
-    const result = await client
-      .db("StudentMentorAPI")
-      .collection("mentor")
-      .find({})
-      .toArray();
-  
-    result ? res.send(result) : res.status(404).send("Mentor Data not Found");
-  });
+  const result = await client
+    .db("StudentMentorAPI")
+    .collection("mentor")
+    .find({})
+    .toArray();
+
+  result ? res.send(result) : res.status(404).send("Mentor Data not Found");
+});
 
 app.post("/createStudent", async function (req, res) {
   const studentData = req.body;
-  console.log(data);
+  console.log(studentData);
   const result = await client
     .db("StudentMentorAPI")
     .collection("student")
     .insertMany(studentData);
-    studentData
+  studentData
     ? res.send(result)
     : res.send({ message: "Student data was not added" });
 });
 
 app.post("/createMentor", async function (req, res) {
-    const mentorData = req.body;
-    console.log(data);
-    const result = await client
-      .db("StudentMentorAPI")
-      .collection("mentor")
-      .insertMany(mentorData);
-      mentorData
-      ? res.send(result)
-      : res.send({ message: "Mentor data was not added" });
-  });
+  const mentorData = req.body;
+  console.log(mentorData);
+  const result = await client
+    .db("StudentMentorAPI")
+    .collection("mentor")
+    .insertMany(mentorData);
+  mentorData
+    ? res.send(result)
+    : res.send({ message: "Mentor data was not added" });
+});
 
+app.put("/assignMentor", async function (req, res) {
+  const Mentor = req.body;
 
-  app.put("/assignMentor", async function (request, response) {
-    const Mentor = request.body;
-  
-    const findStudent = await client
-      .db("StudentMentorAPI")
-      .collection("student")
-      .findOne({ MentorName: "" });
-  
-    const Student = findStudent.StudentName;
-    const findMentor = await client
-      .db("StudentMentorAPIb39we")
-      .collection("mentor")
-      .findOne({ MentorName: Mentor.MentorName });
-  
-    const mentorName = { MentorName: Mentor.MentorName };
-    console.log(findMentor);
-    findMentor.Students =
-      typeof findMentor.Students === typeof []
-        ? [Student]
-        : findMentor.Students.push(Student);
-    console.log(Student);
-    const updateMentor = await client
-      .db("StudentMentorAPI")
-      .collection("mentor")
-      .updateOne({ MentorName: Mentor.MentorName }, { $set: findMentor });
-  
-    const updateStudent = await client
-      .db("StudentMentorAPI")
-      .collection("student")
-      .updateOne({ StudentName: Student }, { $set: mentorName });
-    response.send(updateMentor);
-  });
+  const findStudent = await client
+    .db("StudentMentorAPI")
+    .collection("student")
+    .findOne({ MentorName: "" });
 
+  const Student = findStudent.StudentName;
+  const findMentor = await client
+    .db("StudentMentorAPIb39we")
+    .collection("mentor")
+    .findOne({ MentorName: Mentor.MentorName });
+
+  const mentorName = { MentorName: Mentor.MentorName };
+  console.log(findMentor);
+  findMentor.Students =
+    typeof findStudent.Students === typeof []
+      ? [Student]
+      : findStudent.Students.push(Student);
+  console.log(Student);
+  const updateMentor = await client
+    .db("StudentMentorAPI")
+    .collection("mentor")
+    .updateOne({ MentorName: Mentor.MentorName }, { $set: findMentor });
+
+  const updateStudent = await client
+    .db("StudentMentorAPI")
+    .collection("student")
+    .updateOne({ StudentName: Student }, { $set: mentorName });
+  res.send(updateMentor);
+});
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT}`));
